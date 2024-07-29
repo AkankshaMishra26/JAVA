a3c8fd8a148c7461bea2b782fe7899050a7297c5
AkankshaMishra26 <114493415+AkankshaMishra26@users.noreply.github.com>
2024-07-29 18:17:57 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,36 @@
+// FOR LOOP
+
+/*public class Loops {
+    public static void main(String args[]) {
+        // for (int i = 0; i <= 5; i++)
+        // System.out.println("Hello Worlds");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}*/
+
+// WHILE LOOP
+/*public class Loops {
+    public static void main(String args[]) {
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}*/
+
+// DO WHILE LOOP
+public class Loops {
+    public static void main(String args[]) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+
+    }
+}
