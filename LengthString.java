b4229de8ee5770c7feb8742e4e93e65f5fb657ac
AkankshaMishra26 <114493415+AkankshaMishra26@users.noreply.github.com>
2024-08-07 10:49:35 +0530

    Add files via upload
    To find Length of Strings.

---
+++ LengthString.java
@@ -0,0 +1,8 @@
+public class LengthString {
+    public static void main(String args[]) {
+        String firstname = "Chinki";
+        String lastname = "Mishra";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname.length());
+    }
+}
