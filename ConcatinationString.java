269f5d8a62f9ec4edb95972ab7c6f74da331fb20
AkankshaMishra26 <114493415+AkankshaMishra26@users.noreply.github.com>
2024-08-07 10:50:34 +0530

    Add files via upload
    Concatination String Function.

---
+++ ConcatinationString.java
@@ -0,0 +1,8 @@
+public class ConcatinationString {
+    public static void main(String args[]) {
+        String firstname = "Chinki";
+        String lastname = "Mishra";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+    }
+}
